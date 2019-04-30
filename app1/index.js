029ad1b7f6ceaf1ba37082da9c617b87ca6bcc1f
Emerson Brancher <Brancher81@gmail.com>
2019-04-30 11:16:59 -0200

    app

---
+++ app1/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View, Button, AppRegistry } from 'react-native';
+
+const gerarNumeroAleatorio = () => {
+	var numero_aleatorio = Math.random();
+
+	numero_aleatorio = Math.floor(numero_aleatorio * 10);
+	alert(numero_aleatorio);
+}
+
+
+const App = () => {
+
+	return (
+		<View>
+			<Text>Gerador de números randômicos!</Text>
+			<Button
+				title="Gerar um número randômico"
+				onPress={gerarNumeroAleatorio}
+			/>
+		</View>
+		);
+};
+
+AppRegistry.registerComponent('app1', () =>  App );
